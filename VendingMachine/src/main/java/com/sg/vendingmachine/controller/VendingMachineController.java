84afb6786a919b614b9587f143cec55d0f95728b
Teresa de Castro <decastro.ter@gmail.com>
2022-07-17 15:49:09 -0400

    Added menuSelection

--- VendingMachine/src/main/java/com/sg/vendingmachine/controller/VendingMachineController.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/controller/VendingMachineController.java
@@ -5,10 +5,31 @@
  */
 package com.sg.vendingmachine.controller;
 
+import com.sg.vendingmachine.ui.UserIO;
+import com.sg.vendingmachine.ui.UserIOConsoleImpl;
+
 /**
  *
  * @author Teresa
  */
 public class VendingMachineController {
     
+    private UserIO io = new UserIOConsoleImpl();
+    
+    // The program should display all of the items and their respective prices when the program starts, along with an option to exit the program.
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing){
+            io.print("=== Candy Machine ===");
+            io.print("1. Tolberone: $2.00");
+            io.print("2. Reese's: $1.75");
+            io.print("3. Kit-Kat: $1.50");
+            io.print("4. Peach Rings: $1.25");
+            io.print("5. Jelly Beans: $1.00");
+            io.print("6. Exit");
+            
+            menuSelection = io.readInt("Please select from the" + " above choices.", 1, 6 );
+        }
+    }
 }
