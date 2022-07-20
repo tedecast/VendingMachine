bb40c1393b11fccd652a01d0be693c0dc5db5c33
Teresa de Castro <decastro.ter@gmail.com>
2022-07-20 14:00:10 -0400

    fixed view - controller

--- VendingMachine/src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
@@ -54,23 +54,23 @@ public class VendingMachineView {
         emptyLine();
     }
     
-    public int displayBuyCandyOptions(Candy candy, List<Candy> candyList) {
-        
-        Float money = io.readFloat("How much money do you have to spend?");
-        BigDecimal userMoney = new BigDecimal(money);
-        
-        while(candy.getCandyQuantity() == 0) {
-            io.print("Sorry we're out of stock of " + candy.getCandyName());
-            displayCandyList(candyList);
-            getCandyNumberChoice();
-            
-        } if (candy.getCandyQuantity() > 0) { //&& moneyBD < candy.getCandyPrice()) {
-            io.print("Insufficient funds. You only put in " + userMoney + 
-                    " and the cost is " + candy.getCandyPrice());
-            io.print("Here's your $" + userMoney + " back");
-            getHitEnter();
-        }
-    }
+//    public int displayBuyCandyOptions(Candy candy, List<Candy> candyList) {
+//        
+//        Float money = io.readFloat("How much money do you have to spend?");
+//        BigDecimal userMoney = new BigDecimal(money);
+//        
+//        while(candy.getCandyQuantity() == 0) {
+//            io.print("Sorry we're out of stock of " + candy.getCandyName());
+//            displayCandyList(candyList);
+//            getCandyNumberChoice();
+//            
+//        } if (candy.getCandyQuantity() > 0) { //&& moneyBD < candy.getCandyPrice()) {
+//            io.print("Insufficient funds. You only put in " + userMoney + 
+//                    " and the cost is " + candy.getCandyPrice());
+//            io.print("Here's your $" + userMoney + " back");
+//            getHitEnter();
+//        } else if (candy.getCandyQuantity() > 0 && u)
+//    }
     
     public void emptyLine() {
         io.print("");
